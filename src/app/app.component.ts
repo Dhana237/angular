c55f4d7fc74b1407c5ff6e7f71afa19e383ca3a0
Dhana237 <dhanalakshmi763927@gmail.com>
2024-12-19 19:14:40 +0530

    deploy netlify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject } from '@angular/core';
+import { HomeComponent } from "./home/home.component";
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'yassh-pharma';
+  constructor( @Inject(DOCUMENT) private document: Document) { }
+  reload(){
+    this.document.location.reload();
+  }
 }
+
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+   standalone: true,
+    imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private router: Router, private location: Location, private aroute: ActivatedRoute) { 
+  }
+  
+  ngOnInit(): void {
+  }
+
+
+
+}
